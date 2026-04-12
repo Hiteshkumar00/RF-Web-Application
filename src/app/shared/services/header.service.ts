25b9e4c77d0ace8fdfd3c2ad9cd3bf6f0ba42f3e
HP <HP@DESKTOP-3TJ85N2>
2026-04-12 16:23:08 +0530

    feat: implement header service for dynamic logo and title management with environment configurations

--- src/app/shared/services/header.service.ts
+++ src/app/shared/services/header.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeaderService {
-    private defaultLogo = 'https://image2url.com/r2/default/images/1773590180920-3a186391-6a15-4289-8b28-223340a67f83.jpg';
-    private defaultTitle = 'RF Application';
+    private defaultLogo = environment.defaultAppLogo;
+    private defaultTitle = environment.defaultAppName;
 
     private logoSource = new BehaviorSubject<string>(this.defaultLogo);
     private titleSource = new BehaviorSubject<string>(this.defaultTitle);
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5196/api'
+    apiUrl: 'http://localhost:5196/api',
+    defaultAppLogo: 'https://images.unsplash.com/photo-1665042099439-39d93c1117e6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fGFuaW1lfGVufDB8fDB8fHww',
+    defaultAppName: 'Ramdev Furniture (Dev)'
 };
